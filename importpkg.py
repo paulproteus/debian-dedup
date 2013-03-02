aada0b97321a336a841385923145e1854fb36860
Helmut Grohne <helmut@subdivi.de>
2013-03-02 21:18:14 +0100

    move sql schema to a separate file

--- importpkg.py
+++ importpkg.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python
-"""
-CREATE TABLE package (package TEXT PRIMARY KEY, version TEXT, architecture TEXT);
-CREATE TABLE content (package TEXT, filename TEXT, size INTEGER, function TEXT, hash TEXT, FOREIGN KEY (package) REFERENCES package(package));
-CREATE TABLE dependency (package TEXT, required TEXT, FOREIGN KEY (package) REFERENCES package(package), FOREIGN KEY (required) REFERENCES package(package));
-CREATE INDEX content_package_index ON content (package);
-CREATE INDEX content_hash_index ON content (hash);
-"""
 
 import hashlib
 import sqlite3
